8c3c999c52f0192eaf0a88d2b1026b2502794644
Luis Martinez <martinez.s.luis@gmail.com>
2018-11-25 13:00:25 +0100

    Updates

---
+++ precache-manifest.a7cda29471283396ff126c86437624be.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/trivial-web/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "bb25da09d18288756969",
+    "url": "/trivial-web/static/css/main.676fe3f4.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/trivial-web/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "58310fc48e79717aeada",
+    "url": "/trivial-web/static/js/1.58310fc4.chunk.js"
+  },
+  {
+    "revision": "7fa6bedf4a92e3303bfe",
+    "url": "/trivial-web/static/js/runtime~main.7fa6bedf.js"
+  },
+  {
+    "revision": "bb25da09d18288756969",
+    "url": "/trivial-web/static/js/main.bb25da09.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/trivial-web/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/trivial-web/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/trivial-web/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "58310fc48e79717aeada",
+    "url": "/trivial-web/static/css/1.4b18385f.chunk.css"
+  },
+  {
+    "revision": "a8a3f89556ee055e46bc140f3bd6655e",
+    "url": "/trivial-web/index.html"
+  }
+];
